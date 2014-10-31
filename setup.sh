88928e1439b06ff6c02395e376da89dc135da6ae
Julien DAUPHANT <judauphant@gmail.com>
2014-11-01 00:35:32 +0100

    Improve setup script

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ sudo apt-get install -y git python-pip python-dev
 sudo pip install ansible
 git clone https://github.com/jdauphant/ansible-ubuntu-desktop.git
 cd ansible-ubuntu-desktop
-sed "s/(git@)?github.com:/https:\/\/github.com\//" .gitmodules > .gitmodules_new
+sed "s/git@github.com:/https:\/\/github.com\//" .gitmodules > .gitmodules_new
 mv .gitmodules_new .gitmodules
 git submodule init
 git submodule update
