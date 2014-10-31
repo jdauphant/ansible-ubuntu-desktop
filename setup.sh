dac94e6c618a47060cdc64c415ce38b2b3023d19
Julien DAUPHANT <judauphant@gmail.com>
2014-11-01 00:34:07 +0100

    Improve setup script

--- setup.sh
+++ setup.sh
@@ -2,4 +2,9 @@
 sudo apt-get install -y git python-pip python-dev
 sudo pip install ansible
 git clone https://github.com/jdauphant/ansible-ubuntu-desktop.git
+cd ansible-ubuntu-desktop
+sed "s/(git@)?github.com:/https:\/\/github.com\//" .gitmodules > .gitmodules_new
+mv .gitmodules_new .gitmodules
+git submodule init
+git submodule update
 ansible-playbook installation.yml --sudo -K -c local -i "localhost,"
